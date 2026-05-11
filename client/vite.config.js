ff5f0d8d774090a76c5bb9b15f909e333a55e8cf
大彭 <dapeng@linmeng.com>
2026-05-11 23:00:52 +0800

    backup: 2026-05-11 23:00 自动备份

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0', // 允许局域网访问
-    port: 5173,
+    port: 5174,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
